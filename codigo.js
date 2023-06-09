5f7a25fd2a55c44eef8f956f77bb481310a948f0
root <root@Ubuntu-Win11>
2023-06-09 16:56:51 -0500

    feat: init proyect

--- codigo.js
+++ codigo.js
@@ -1,56 +1,19 @@
-// Lista de estados posibles para una transferencia.
-const statuses = [
-    {
-        id: 1,
-        name: 'CREADA'
-    },
-    {
-        id: 2,
-        name: 'PAGADA'
-    },
-    {
-        id: 3,
-        name: 'ABORTADA'
-    },
-    {
-        id: 4,
-        name: 'FINALIZADA'
-    }
-];
+import Transfer from "./models/transfer.js";
+import TransferSystem from "./service/TransferSystemService.js";
+import { initDataTransfers } from "./utils/data.js";
 
-// Lista de transferencias inicial
-const transfers = [
-    {
-        id: 1,
-        licensePlate: 'LFTS34',
-        email: 'usuario1@autored.cl',
-        status: 'CREADA',
-    },
-    {
-        id: 2,
-        licensePlate: 'LFTS35',
-        email: 'usuario1@autored.cl',
-        status: 'ABORTADA',
-    },
-    {
-        id: 3,
-        licensePlate: 'BDLS99',
-        email: 'usuario3@autored.cl',
-        status: 'CREADA',
-    },
-    {
-        id: 4,
-        licensePlate: 'LFTS34',
-        email: 'usuario4@autored.cl',
-        status: 'CREADA',
-    },
-    {
-        id: 5,
-        licensePlate: 'BDLS99',
-        email: 'usuario5@autored.cl',
-        status: 'FINALIZADA',
-    }
-];
+function main() {
 
+    const transferSystem = new TransferSystem();
 
-// Escribe tu codigo acá 
+    const newTransfer1 = new Transfer('usuario1@autored.cl','LFTS35');
+    
+    const response1 = transferSystem.createTransfer(newTransfer1);
+
+    console.log(response1);
+
+    const payTransfer = transferSystem.payTransfer('usuario1@autored.cl','LFTS35');
+    console.log(payTransfer);
+}
+
+main();
